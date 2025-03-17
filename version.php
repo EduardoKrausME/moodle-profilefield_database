b9a2bddd7a6bc4da812c1d1bb53d9b979a0ce809
Eduardo Kraus <kraus@eduardokraus.com>
2025-03-17 10:05:19 -0300

    V 1.1.7

--- version.php
+++ version.php
@@ -24,10 +24,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025031700;
+$plugin->version = 2025031701;
 $plugin->requires = 2022112800;
 $plugin->component = "profilefield_database";
-$plugin->release = "1.1.6";
+$plugin->release = "1.1.7";
 $plugin->maturity = MATURITY_STABLE;
 
 $plugin->dependencies = [
